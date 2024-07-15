a4d05ea96662c955ff96b531e978531a70480ace
Eracles <camposzavalaeracles@gmail.com>
2024-07-14 23:26:20 -0600

    actualizacion nueva y correccion de errores

--- script.js
+++ script.js
@@ -7,53 +7,57 @@ const barras = document.querySelector('.tres-barras')
 let movido = false
 let header_oculto = false
 
-containerClickme.addEventListener('click', function() {
+// Funcion para mover la foto y el texto
+containerClickme.addEventListener('click', function () {
     if (!movido) {
         imagen_eracles.style.transform = 'translateX(-200px)'
         containerClickme.style.transform = 'translateX(-200px)'
         texto_eracles.classList.add('texto-visible')
-        texto_eracles.style.right='0'
+        texto_eracles.style.right = '0'
     } else {
         imagen_eracles.style.removeProperty('transform')
         containerClickme.style.removeProperty('transform')
-        texto_eracles.style.right='150px'
+        texto_eracles.style.right = '150px'
         texto_eracles.classList.remove('texto-visible')
     }
-    
-    movido = !movido
-});
 
-barras.addEventListener('click', function() {
+    movido = !movido
+})
+// funcion para ocultar el header
+barras.addEventListener('click', function () {
     if (!header_oculto) {
         header.style.transform = 'translateX(0)'
     } else {
         header.style.transform = 'translateX(-40vw)'
     }
-    
+
     header_oculto = !header_oculto
-});
+})
+// Verificar si el ancho de la pantalla es el correcto para ocultar el header cuando se hacen clicks 
+// fuera del header
+if (window.innerWidth < 576) {
+    document.addEventListener('click', function (e) {
+        const clickfuera = barras.contains(e.target) || header.contains(e.target);
 
-document.addEventListener('click', function(e) {
-    const clickfuera = barras.contains(e.target) || header.contains(e.target);
-    
-    if (!clickfuera) {
-        header.style.transform = 'translateX(-40vw)';
-    }
-});
-
-containerClickme.addEventListener('mouseover',function(){
-    clickme.style.opacity='1'
+        if (!clickfuera) {
+            header.style.transform = 'translateX(-40vw)';
+        }
+    })
+}
+// funcion para el hover de la imagen
+containerClickme.addEventListener('mouseover', function () {
+    clickme.style.opacity = '1'
     imagen_eracles.style.filter = 'brightness(50%)'
 })
-containerClickme.addEventListener('mouseout',function(){
-    clickme.style.opacity='0'
+containerClickme.addEventListener('mouseout', function () {
+    clickme.style.opacity = '0'
     imagen_eracles.style.removeProperty('filter')
 })
-
-window.addEventListener('resize', function(){
-    if(window.innerWidth<576){
+// funcion para mostrar el boton de barras 
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 576) {
         barras.classList.remove('hidden')
     }
     else barras.classList.add('hidden'); header.style.removeProperty('transform')
-});
+})
 
